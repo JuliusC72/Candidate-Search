107f53a3babe1ed10facde0c456a4470e932ba83
JuliusC72 <72jules@gmail.com>
2025-04-18 11:32:40 -0600

    allowed render as host

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+//https://vitejs.dev/config/
 export default defineConfig({
-  envDir: './env',
   plugins: [react()],
+  preview: {
+    port: Number(process.env.PORT) || 4173,
+    host: true,
+    allowedHosts: ['candidate-search-aa5x.onrender.com', '.onrender.com']
+  }
 });
